5f3b8b4d726d7eba88317c3621d5cf10d4a8bc03
slemden <slemnem@naver.com>
2024-01-20 20:50:28 +0900

    nextjs practice

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
 import { lusitana } from  '@/app/ui/fonts'
 import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
 export default function Page() {
   return (
